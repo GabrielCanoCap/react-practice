cdfea0111a4f14d4f904abb3d2485072479820d5
CanoGabriel <gabrielcano@hotmail.fr>
2020-06-28 20:53:31 +0200

    Front: eslint

--- front/src/app/app.js
+++ front/src/app/app.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { InputPageExample } from "./domains/example";
 
-import './app.scss';
+import "./app.scss";
 
 const App = () => (
   <div className="app">
